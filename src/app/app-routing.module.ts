06341226533cbd31c595c9246c774d41272ca0d7
Taurai Chiwawa <tauraishep@gmail.com>
2023-12-06 14:40:02 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { MainComponent } from './Materials/main/main.component';
+import { DashboardComponent } from './Materials/dashboard/dashboard.component';
+import { AppointmentComponent } from './Materials/appointment/appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      { path: 'appointment', component:AppointmentComponent}
+    ],
+  },
+  { path: '', component:LoginComponent},
+  { path: 'register', component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HeaderComponent } from './Layouts/header/header.component';
+import { DashboardComponent } from './Materials/dashboard/dashboard.component';
+import { MainComponent } from './Materials/main/main.component';
+import { SidebarComponent } from './Layouts/sidebar/sidebar.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
+import { AppointmentComponent } from './Materials/appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    DashboardComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
